f718ac84c6a9a1bb2e7e0b949e5f969f550623f2
rymbln <rymbln@gmail.com>
2015-02-04 02:03:43 +0300

    First realease

---
+++ Karantin/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Karantin
+{
+    partial class Form1
+    {
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(12, 106);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(133, 52);
+            this.downloadButton.TabIndex = 0;
+            this.downloadButton.Text = "Начать поиск";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 164);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 132);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1000";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Какую цену ищем";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Период опроса (секунд)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 83);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(181, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Ищем только незанятые игры";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 52);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Остановить поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 308);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.downloadButton);
+            this.Name = "Form1";
+            this.Text = "KARANTIN FINDER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
---
+++ Karantin/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Xml;
+using HtmlAgilityPack;
+
+namespace Karantin
+{
+    public partial class Form1 : Form
+    {
+
+
+        private BackgroundWorker backgroundWorker1;
+        private Button downloadButton;
+        private string htmlCode;
+        private string time;
+        private string date;
+        private string price;
+        private bool found;
+        private bool findFreeTime;
+        private System.Timers.Timer timer;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            price = textBox2.Text + ".00";
+    
+
+            // Instantiate BackgroundWorker and attach handlers to its
+            // DowWork and RunWorkerCompleted events.
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+        }
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            // Start the download operation in the background.
+            this.backgroundWorker1.RunWorkerAsync();
+
+            // Disable the button for the duration of the download.
+            //  this.downloadButton.Enabled = false;
+
+            // Once you have started the background thread you 
+            // can exit the handler and the application will 
+            // wait until the RunWorkerCompleted event is raised.
+
+            // Or if you want to do something else in the main thread,
+            // such as update a progress bar, you can do so in a loop 
+            // while checking IsBusy to see if the background task is
+            // still running.
+
+            while (this.backgroundWorker1.IsBusy)
+            {
+                //  progressBar1.Increment(1);
+                // Keep UI messages moving, so the form remains 
+                // responsive during the asynchronous operation.
+                Application.DoEvents();
+            }
+
+            SetTextSafe("Last scan: " + DateTime.Now
+                + " - Date: " + (string.IsNullOrEmpty(date) ? "none" : date).ToString()
+                + " - Time: " + (string.IsNullOrEmpty(time) ? "none" : time).ToString());
+        }
+
+
+        private void SetTextSafe(string text)
+        {
+            if (InvokeRequired)
+                this.BeginInvoke(new Action<string>((s) =>
+                {
+                    SetText(s);
+                }), text);
+            else SetText(text);
+        }
+
+        private void SetText(string text)
+        {
+            this.textBox1.Text = text;
+            if (found)
+            {
+                timer.Enabled = false;
+                MessageBox.Show(this.textBox1.Text, "МЫ НАШЛИ!",
+MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+         
+     
+        }
+
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+
+            // Get fin type
+            findFreeTime = this.checkBox1.Checked;
+
+            // Create a timer 
+            timer = new System.Timers.Timer(int.Parse(this.textBox3.Text) * 1000);
+
+            // Hook up the Elapsed event for the timer.
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            timer.Enabled = true;
+
+
+        }
+
+        private void backgroundWorker1_DoWork(
+            object sender,
+            DoWorkEventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                client.Encoding = UTF8Encoding.UTF8;
+                htmlCode = client.DownloadString("http://karantinum.ru/quests/yantar/");
+
+                HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+
+                document.LoadHtml(htmlCode);
+                HtmlNodeCollection collection = document.DocumentNode.SelectNodes("//div");
+                if (findFreeTime)
+                {
+                    var findclasses = document.DocumentNode.Descendants("div")
+                             .Where(d =>
+             d.Attributes.Contains("class") && d.Attributes["class"].Value.Contains("booking_hour") && d.Attributes["class"].Value != "booking_hour busy"
+             && d.Attributes.Contains("price") && d.Attributes["price"].Value.Contains(price)
+         );
+                    if (findclasses.Count() > 0)
+                    {
+                        time = findclasses.FirstOrDefault().InnerText;
+
+                        var dateDiv = findclasses.FirstOrDefault().ParentNode.ParentNode.Descendants("div")
+                            .Where(d => d.Attributes.Contains("class") && d.Attributes["class"].Value.Contains("booking_date_f"));
+
+                        byte[] bytes = Encoding.Convert(Encoding.GetEncoding("windows-1251"), Encoding.UTF8, Encoding.GetEncoding("windows-1251").GetBytes(dateDiv.FirstOrDefault().InnerText));
+
+                        date = Encoding.UTF8.GetString(bytes).Trim();
+                        found = true;
+                    }
+                    else
+                    {
+                        date = null;
+                        time = null;
+                        found = false;
+                    } 
+                }
+                else
+                {
+                    var findclasses = document.DocumentNode.Descendants("div")
+                             .Where(d =>
+             d.Attributes.Contains("class") && d.Attributes["class"].Value.Contains("booking_hour") 
+             && d.Attributes.Contains("price") && d.Attributes["price"].Value.Contains(price)
+         );
+                    if (findclasses.FirstOrDefault().InnerText != null)
+                    {
+                        time = findclasses.FirstOrDefault().InnerText;
+
+                        var dateDiv = findclasses.FirstOrDefault().ParentNode.ParentNode.Descendants("div")
+                            .Where(d => d.Attributes.Contains("class") && d.Attributes["class"].Value.Contains("booking_date_f"));
+
+                        byte[] bytes = Encoding.Convert(Encoding.GetEncoding("windows-1251"), Encoding.UTF8, Encoding.GetEncoding("windows-1251").GetBytes(dateDiv.FirstOrDefault().InnerText));
+
+                        date = Encoding.UTF8.GetString(bytes).Trim();
+                        found = true;
+                    }
+                    else
+                    {
+                        date = null;
+                        time = null;
+                        found = false;
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(
+            object sender,
+            RunWorkerCompletedEventArgs e)
+        {
+            // Set progress bar to 100% in case it's not already there.
+            //   progressBar1.Value = 100;
+
+            if (e.Error == null)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Failed to download file",
+                    "Download failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            // Enable the download button and reset the progress bar.
+            this.downloadButton.Enabled = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+        }
+    }
+}
